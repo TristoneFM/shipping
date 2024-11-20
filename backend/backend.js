4fe09dc609773e111b78a902af60385c2b162b9c
Francisco Morales <francisco.morales@tristone.com>
2024-11-20 10:44:33 -0600

    fix: :white_check_mark: run on IIS

--- backend/backend.js
+++ backend/backend.js
@@ -8,14 +8,14 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const app = express()
 
-
+const portNumber = process.env.port || process.env.PORT || 5001
 const routes = require('./routes/routes_RFC')
 const routesDB = require('./routes/routes_DB')
 const routesAD = require('./routes/routes_AD')
 
 
 const update_dock = require('../backend/routines/routine_dock').UPDATE()// Update dock routine do not remove
-const portNumber = process.env.port || process.env.PORT || 5000
+// const portNumber = 5001
 
 app.use(cors({ origin: true, credentials: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
--- frontend/server.js
+++ frontend/server.js
@@ -1,27 +1,56 @@
-const https = require('https');
-const { parse } = require('url');
-const next = require('next');
-const fs = require('fs');
-const path = require('path');
+// const https = require('https');
+// const { parse } = require('url');
+// const next = require('next');
+// const fs = require('fs');
+// const path = require('path');
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const dev = process.env.NODE_ENV !== 'DEV';
+// const port = process.env.PORT || 3001;
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
+
+// // Load SSL key and certificate
+// const httpsOptions = {
+//   key: fs.readFileSync(path.resolve('certificates/key.pem')),
+//   cert: fs.readFileSync(path.resolve('certificates/cert.pem')),
+// };
+
+// app.prepare().then(() => {
+//   https.createServer(httpsOptions, (req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     handle(req, res, parsedUrl);
+//   }).listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on https://localhost:${port}`);
+//   });
+// });
+
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3001;
-
-// Load SSL key and certificate
-const httpsOptions = {
-  key: fs.readFileSync(path.resolve('certificates/key.pem')),
-  cert: fs.readFileSync(path.resolve('certificates/cert.pem')),
-};
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  https.createServer(httpsOptions, (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
   }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on https://localhost:${port}`);
-  });
-});
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
